4029f4723c114827d426eebaf1039a630724a443
tanya-flip <tanya.sravan@flipkart.com>
2026-06-03 06:44:49 +0530

    stopping read audits

--- server/src/main/java/com/flipkart/grayskull/controllers/SecretBatchController.java
+++ server/src/main/java/com/flipkart/grayskull/controllers/SecretBatchController.java
@@ -71,7 +71,7 @@ public class SecretBatchController {
                     .metadata(metadata)
                     .build();
 
-            asyncAuditLogger.log(auditEntry);
+//            asyncAuditLogger.log(auditEntry);
         }
     }
 }
